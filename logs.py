566e73ca742b760295ebeba728b0dae513f13a76
Mandar Waghe <waghemandar1712@gmail.com>
2018-09-04 12:29:37 +0530

    reduce the length of lines which is greater then 179 spaces

--- logs.py
+++ logs.py
@@ -4,13 +4,22 @@ import psycopg2
 DB_NAME = "news"
 
 # 1. What are the most popular three articles of all time?
-query1 = """SELECT articles.title,count(*) AS views FROM articles,log WHERE log.path LIKE concat('%',articles.slug) group by articles.title order by views desc limit 3""";
+query1 = """SELECT articles.title,count(*) AS views FROM articles,\
+            log WHERE log.path LIKE concat('%',articles.slug) \
+            GROUP BY articles.title \
+            ORDER BY views DESC LIMIT 3""";
 
 # 2. Who are the most popular article authors of all time?
-query2 = """SELECT authors.name,count(*) AS num FROM articles,log,authors WHERE log.path LIKE concat('%',articles.slug) and articles.author=authors.id group by authors.name order by num desc""";
+query2 = """SELECT authors.name,count(*) AS num FROM articles,log,authors\
+            WHERE log.path LIKE concat('%',articles.slug)\
+            AND articles.author=authors.id \
+            GROUP BY authors.name \
+            ORDER BY num desc""";
 
 # 3. On which days did more than 1% of requests lead to errors?
-query3 = """SELECT TO_CHAR(date :: DATE,'Mon dd,yyyy'),percent_error FROM percentage WHERE percent_error > 1""";
+query3 = """SELECT TO_CHAR(date :: DATE,'Mon dd,yyyy'),percent_error \
+            FROM percentage \
+            WHERE percent_error > 1""";
 
 # to store results
 query_1_result = dict()
@@ -28,16 +37,19 @@ def get_query_result(query):
     db.close()
     return results
 
+
 def print_articles_query_results(query_result):
     print ("""\n1. The 3 most popular articles of all time are:\n""")
     for result in query_result['results']:
         print ('\t "' + str(result[0]) + '" --- ' + str(result[1]) + ' views')
+
 
 def print_author_query_results(query_result):
     print ("""\n2. The most popular article authors of
     all time are:\n""")
     for result in query_result['results']:
         print ('\t ' + str(result[0]) + ' --- ' + str(result[1]) + ' views')
+        
 
 def print_error_query_results(query_result):
     print ("""\n3. Days with more than 1% of request that
